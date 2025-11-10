e24381b60446957dfa4f87ccb7bd580173f6e6a0
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-11-10 20:15:54 +0600

    update: api client

--- src/utils/api/api.client.ts
+++ src/utils/api/api.client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosError } from 'axios'
 
 const baseURL = typeof window === 'undefined' 
-    ? process.env.NEXTAUTH_URL || 'http://localhost:3000' 
+    ? process.env.NEXTAUTH_URL
     : '';
 
 const api: AxiosInstance = axios.create({
