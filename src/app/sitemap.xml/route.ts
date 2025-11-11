55fbd9477f2063cc47b65c461f233713ab017089
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-11-11 15:26:38 +0600

    updated sitemap and robots

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,29 +1,15 @@
 // /app/sitemap.xml/route.ts
-import { NextRequest } from "next/server";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(req: NextRequest) {
+export async function GET() {
   const baseUrl = process.env.NEXT_PUBLIC_DOMAIN ?? "https://next-learn-nu-olive.vercel.app";
-
-  // List your routes here
   const routes = ["", "/about", "/features", "/how-to-use"];
-
-  // Use only the date part for <lastmod> to reduce unnecessary changes
   const today = new Date().toISOString().split("T")[0];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns:xhtml="http://www.w3.org/1999/xhtml">
-${routes
-      .map(
-        (route) => `
-  <url>
-    <loc>${baseUrl}${route}</loc>
-    <lastmod>${today}</lastmod>
-  </url>`
-      )
-      .join("")}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${routes.map(route => `<url>
+  <loc>${baseUrl}${route}</loc>
+  <lastmod>${today}</lastmod>
+</url>`).join('')}
 </urlset>`;
 
   return new Response(sitemap, {
